0cc62315a675b046dabce82a6c40b7d5bc57e508
Harsh kumrawat <harshkumrawat@gmail.com>
2024-09-06 19:59:23 +0530

    'few updates for user module'

--- src/app/[lang]/(pages)/user/Api/useUserApi.js
+++ src/app/[lang]/(pages)/user/Api/useUserApi.js
@@ -1,60 +1,38 @@
 // 'use client'
 import { useEffect, useState } from 'react'
-
+import { ApiRequestHandle } from '@/libs/axios'
 import axios from 'axios'
 
 import { USER_MODULE_ENDPOINTS } from '../Const/ApiEndpoints'
 
 export default function useUserApi() {
   const [data, setData] = useState([])
-
-  const fetchData = () => {
+  const [loader, setLoader] = useState(true)
+  const fetchData = async () => {
     try {
-      axios.get(`${USER_MODULE_ENDPOINTS}`)?.then(res => {
-        setData(res?.data)
+      const endpoint = `${process.env.NEXT_PUBLIC_LMS_API_URL}users/list` // Construct the full URL
+
+      const response = await axios.get(endpoint, {
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_LMS_TOKEN}`, // Add Authorization header
+          Network: process.env.NEXT_PUBLIC_LMS_TOKEN, // Add Network header
+          Accept: 'application/json' // Specify the accepted response format
+        }
       })
+      setLoader(false)
+      setData(response.data?.payload?.data) // Update the state with the fetched data
     } catch (error) {
       console.error('Error fetching data:', error)
     }
   }
-
   useEffect(() => {
     fetchData()
   }, [])
 
-  const addUsersData = userData => {
-    try {
-      axios.post(`${USER_MODULE_ENDPOINTS}`, userData).then(() => fetchData())
-
-      //   return response.data
-    } catch (error) {
-      console.error('Error:', error)
-    }
-  }
-
-  const updateUsersData = (userId, userData) => {
-    try {
-      axios.put(`${USER_MODULE_ENDPOINTS}/${userId}`, userData).then(() => fetchData())
-
-      //   return response.data
-    } catch (error) {
-      console.error('Error:', error)
-    }
-  }
-
-  const deleteUserData = userId => {
-    try {
-      axios.delete(`${USER_MODULE_ENDPOINTS}/${userId}`).then(() => fetchData())
-    } catch (error) {
-      console.error('Error fetching data:', error)
-    }
-  }
-
   return {
-    deleteUserData,
-    updateUsersData,
-    addUsersData,
     data,
-    setData
+    setData,
+    loader,
+    setLoader
   }
 }
